3eda8a925d81e626b79eb36d272504c79c4a3033
alex <alexnbui@gmail.com>
2023-04-19 22:34:09 -0700

    Fetched match data and reordered files

--- data/fetch.py
+++ data/fetch.py
@@ -19,9 +19,9 @@ def apiCall(url: str):
         response = requests.get(
             url, headers={"X-Riot-Token": riot_api_key})
         if response.status_code == 429:  # API limit reached
-            print("API limit reached. Pausing for 60 seconds...")
+            print("API limit reached. Pausing for 20 seconds...")
             reqCount = 0
-            time.sleep(60)  # Pause for 60 seconds
+            time.sleep(20)  # Pause for 20 seconds
         else:
             api_limit_reached = False  # Exit the loop
 
@@ -31,17 +31,17 @@ def apiCall(url: str):
 print("grabbing players")
 summonerIdQueue = Queue()
 playerJson = apiCall(
-    "https://euw1.api.riotgames.com/tft/league/v1/challenger")
+    "https://na1.api.riotgames.com/tft/league/v1/challenger")
 for summoner in playerJson.get("entries"):
     summonerIdQueue.put(summoner.get("summonerId"))
 
 playerJson = apiCall(
-    "https://euw1.api.riotgames.com/tft/league/v1/grandmaster")
+    "https://na1.api.riotgames.com/tft/league/v1/grandmaster")
 for summoner in playerJson.get("entries"):
     summonerIdQueue.put(summoner.get("summonerId"))
 
 playerJson = apiCall(
-    "https://euw1.api.riotgames.com/tft/league/v1/master")
+    "https://na1.api.riotgames.com/tft/league/v1/master")
 for summoner in playerJson.get("entries"):
     summonerIdQueue.put(summoner.get("summonerId"))
 
@@ -49,7 +49,7 @@ print("getting every puuid")
 summonerPuuidQueue = Queue()
 while not summonerIdQueue.empty():
     summonerJson = apiCall(
-        "https://euw1.api.riotgames.com/tft/summoner/v1/summoners/" + summonerIdQueue.get())
+        "https://na1.api.riotgames.com/tft/summoner/v1/summoners/" + summonerIdQueue.get())
     print(summonerJson.get("puuid"))
     summonerPuuidQueue.put(summonerJson.get("puuid"))
 
@@ -57,23 +57,9 @@ print("getting every match and placing them into a set")
 matchSet = set({})
 while not summonerPuuidQueue.empty():
     matchJson = apiCall(
-        "https://europe.api.riotgames.com/tft/match/v1/matches/by-puuid/" + summonerPuuidQueue.get() + "/ids?count=20")
+        "https://americas.api.riotgames.com/tft/match/v1/matches/by-puuid/" + summonerPuuidQueue.get() + "/ids?count=20")
     for matchId in matchJson:
         matchSet.add(matchId)
 
 f = open("matches.json", "w")
 f.write(json.dumps(list(matchSet)))
-matchIdsQueue = Queue()
-
-for matchId in matchSet:
-    matchIdsQueue.put(matchId)
-matchList = list([])
-while not matchIdsQueue.empty():
-    matchId = matchIdsQueue.get()
-    matchJson = apiCall(
-        "https://europe.api.riotgames.com/tft/match/v1/matches/" + matchId)
-    matchList.append(matchJson)
-    f = open("matches.json", "w")
-    f.write(json.dumps(matchList))
-
-   
